7319b19fcd6c455a4a6f1e43a3dba1315f3678d2
Rhabi Mendoza <111347175+redpmendoza@users.noreply.github.com>
2023-07-05 22:16:19 +0800

    Add files via upload

---
+++ NumberMirror.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Codechef{
+	public static void main (String[] args) throws java.lang.Exception{
+		
+		// Start of Solution
+		Scanner in = new Scanner(System.in);
+		int N = in.nextInt();
+		System.out.print(N);
+		// End of Solution
+		
+	}
+}
