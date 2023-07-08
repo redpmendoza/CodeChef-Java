5c9bf948f1b40281e3c021581f4ab7debd7477cb
Rhabi Mendoza <111347175+redpmendoza@users.noreply.github.com>
2023-07-08 22:24:49 +0800

    Add files via upload

---
+++ Squats.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Squats{
+	public static void main (String[] args) throws java.lang.Exception{
+		
+        // Start of Solution
+        Scanner in = new Scanner(System.in);
+
+        int T = in.nextInt();
+
+        for(int ctr = 0; ctr < T; ctr++){
+            int X = in.nextInt();
+            System.out.println(X * 15);
+        }
+        // End of Solution
+
+	}
+}
