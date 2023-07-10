5efb0b52cc69b96bc86139e580b0469cdf24eaac
Rhabi Mendoza <111347175+redpmendoza@users.noreply.github.com>
2023-07-10 16:40:20 +0800

    Add files via upload

---
+++ WaterConsumption.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class Codechef{
+	public static void main (String[] args) throws java.lang.Exception{
+		
+		// Start of Solution
+		Scanner in = new Scanner(System.in);
+		
+		int T = in.nextInt();
+		
+		for(int ctr = 0; ctr < T; ctr++){
+		    int X = in.nextInt();
+		    
+		    if(X >= 2000){
+		        System.out.println("YES");
+		    }
+		    else{
+		        System.out.println("NO");
+		    }
+		}
+		// End of Solution
+		
+	}
+}
