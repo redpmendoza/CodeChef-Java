32ea9411172646d85f41364894e914437c9d3b76
Rhabi Mendoza <111347175+redpmendoza@users.noreply.github.com>
2023-07-16 22:21:48 +0800

    Add files via upload

---
+++ HowManyUnattemptedProblems.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class HowManyUnattemptedProblems{
+	public static void main (String[] args) throws java.lang.Exception{
+	
+	    // Start of Solution
+	    Scanner in = new Scanner(System.in);
+	    
+	    int X = in.nextInt();
+	    int Y = in.nextInt();
+	    
+	    System.out.println(X-Y);
+	    // End of Solution
+	    
+	}
+}
