0e2f7a2fcf709fd940684a8b4a861313444b08d2
Rhabi Mendoza <111347175+redpmendoza@users.noreply.github.com>
2023-07-18 20:39:16 +0800

    Add files via upload

---
+++ DonationDrive.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class DonationDrive{
+	public static void main (String[] args) throws java.lang.Exception{
+		
+		// Start of Solution
+		Scanner in = new Scanner(System.in);
+		
+		int T = in.nextInt();
+		
+		for(int ctr = 0; ctr < T; ctr++){
+		    int X = in.nextInt();
+		    int Y = in.nextInt();
+		    
+		    System.out.println(X-Y);
+		}
+		// End of Solution
+		
+	}
+}
