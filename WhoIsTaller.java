feb677bd2329b57013fbbed9719066b2ed94b8c4
Rhabi Mendoza <111347175+redpmendoza@users.noreply.github.com>
2023-07-25 12:34:36 +0800

    Create WhoIsTaller.java

---
+++ WhoIsTaller.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class WhoIsTaller{
+	public static void main (String[] args) throws java.lang.Exception{
+		
+		// Start of Solution
+		Scanner in = new Scanner(System.in);
+		
+		int T = in.nextInt();
+		
+		for(int ctr = 0; ctr < T; ctr++){
+		    int X = in.nextInt();
+		    int Y = in.nextInt();
+		    
+		    if(X > Y){
+		        System.out.println("A");
+		    }
+		    else{
+		        System.out.println("B");
+		    }
+		}
+		// End of Solution
+		
+	}
+}
