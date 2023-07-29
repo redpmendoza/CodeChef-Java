fa35a26a1b4eaeef24ba95a8f7d0b7d259de7ef6
Rhabi Mendoza <111347175+redpmendoza@users.noreply.github.com>
2023-07-29 22:49:54 +0800

    Create TwoThousand.java

---
+++ TwoThousand.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class TwoThousand{
+	public static void main (String[] args) throws java.lang.Exception{
+		
+		// Start of Solution
+		Scanner in = new Scanner(System.in);
+		
+        int N = in.nextInt();
+		    
+		System.out.println(((N*2000)/500));
+		// End of Solution
+		
+	}
+}
