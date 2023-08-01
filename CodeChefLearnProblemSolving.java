57f6d0ff44e10eb7e6edb582014678b287bb211f
Rhabi Mendoza <111347175+redpmendoza@users.noreply.github.com>
2023-08-01 22:03:38 +0800

    Create CodeChefLearnProblemSolving.java

---
+++ CodeChefLearnProblemSolving.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class CodeChefLearnProblemSolving{
+	public static void main (String[] args) throws java.lang.Exception{
+	    
+	    // Start of Solution
+	    Scanner in = new Scanner(System.in);
+	    
+	    int N = in.nextInt();
+	    
+	    System.out.println(N*2);
+	    // End of Solution
+	    
+	}
+}
