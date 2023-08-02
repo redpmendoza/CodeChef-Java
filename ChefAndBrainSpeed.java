dd7de033e3e87fa449417b3867a098ec68d2e337
Rhabi Mendoza <111347175+redpmendoza@users.noreply.github.com>
2023-08-02 21:58:02 +0800

    Create ChefAndBrainSpeed.java

---
+++ ChefAndBrainSpeed.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class ChefAndBrainSpeed{
+	public static void main (String[] args) throws java.lang.Exception{
+		
+		// Start of Solution
+		Scanner in = new Scanner(System.in);
+		
+		int X = in.nextInt();
+		int Y = in.nextInt();
+		
+		if(Y > X){
+		    System.out.println("YES");
+		}
+		else{
+		    System.out.println("NO");
+		}
+		// End of Solution
+		
+	}
+}
