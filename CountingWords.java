73cb7fe46134a799abcf0ceff12783714933c277
Rhabi Mendoza <111347175+redpmendoza@users.noreply.github.com>
2023-08-07 21:20:31 +0800

    Create CountingWords.java

---
+++ CountingWords.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class CountingWords{
+	public static void main (String[] args) throws java.lang.Exception{
+		
+		// Start of Solution
+		Scanner in = new Scanner(System.in);
+		
+		int T = in.nextInt();
+		
+		for(int ctr = 0; ctr < T; ctr++){
+		    int N = in.nextInt();
+		    int M = in.nextInt();
+		    
+		    System.out.println(N*M);
+		}
+		// End of Solution
+		
+	}
+}
