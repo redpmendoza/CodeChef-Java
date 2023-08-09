80ead0ee4add04748a782965064e899b670fb35d
Rhabi Mendoza <111347175+redpmendoza@users.noreply.github.com>
2023-08-09 21:59:05 +0800

    Create TomAndJerryChase.java

---
+++ TomAndJerryChase.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class TomAndJerryChase{
+    public static void main (String[] args) throws java.lang.Exception{
+		
+		// Start of Solution
+		Scanner in = new Scanner(System.in);
+		
+		int T = in.nextInt();
+		
+		for(int ctr = 0; ctr < T; ctr++){
+		    int X = in.nextInt();
+		    int Y = in.nextInt();
+		    
+		    if(Y > X){
+		        System.out.println("YES");
+		    }
+		    else{
+		        System.out.println("NO");
+		    }
+		}
+		// End of Solution
+		
+	}
+}
