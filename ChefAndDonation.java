6e11fc8bf23b3a04dc79f70137006c87e0b87e7d
Rhabi Mendoza <rhabidpmendoza@gmail.com>
2023-08-15 22:17:53 +0800

    Create ChefAndDonation.java

---
+++ ChefAndDonation.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class ChefAndDonation{
+	public static void main (String[] args) throws java.lang.Exception{
+		
+		// Start of Solution
+		Scanner in = new Scanner(System.in);
+		
+		int T = in.nextInt();
+		
+		for(int ctr = 0; ctr < T; ctr++){
+		    int X = in.nextInt();
+		    int Y = in.nextInt();
+		    
+		    System.out.println(Y-X);
+		}
+		// End of Solution
+		
+	}
+}
